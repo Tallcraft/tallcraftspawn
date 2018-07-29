bb0a6e0957d7ba15fe17b1180b7b0a45dafb3875
Trikolon <1077860+Trikolon@users.noreply.github.com>
2018-07-29 13:17:10 +0200

    Added configuration, handle player respawn, save player orientation  * Implemented storage of yaw & pitch. closes #1  * Added configuration which stores x,y,z,yaw,pitch for every world  * Added configuration option to overwrite bed respawn location

--- src/main/java/com/tallcraft/tallcraftspawn/TallcraftSpawn.java
+++ src/main/java/com/tallcraft/tallcraftspawn/TallcraftSpawn.java
@@ -1,16 +1,18 @@
 package com.tallcraft.tallcraftspawn;
 
 
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -19,11 +21,14 @@ import java.util.logging.Logger;
 public final class TallcraftSpawn extends JavaPlugin implements Listener {
 
     private final Logger logger = Logger.getLogger(this.getName());
+    private FileConfiguration config;
 
     @Override
     public void onEnable() {
+        initConfig();
         getServer().getPluginManager().registerEvents(this, this);
     }
+
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         if (!(sender instanceof Player)) {
@@ -57,11 +62,70 @@ public final class TallcraftSpawn extends JavaPlugin implements Listener {
         return false;
     }
 
+    @EventHandler
+    public void onPlayerRespawn(final PlayerRespawnEvent e) {
+        if(e.isBedSpawn() && !config.getBoolean("overrideBedSpawn")) {
+            return;
+        }
+        World world = e.getRespawnLocation().getWorld();
+        e.setRespawnLocation(getSpawnLocation(world));
+    }
+
+
     private void toSpawn(Player player) {
-        player.teleport(player.getWorld().getSpawnLocation());
+        World world = player.getWorld();
+        Location location = getSpawnLocation(world);
+        logger.info(location.toString());
+        player.teleport(location);
     }
 
     private void setSpawn(Location location) {
+        // Also set vanilla world spawn
         location.getWorld().setSpawnLocation(location);
+
+        logger.info(location.toString());
+
+        String path = "spawn.worlds." + location.getWorld().getName();
+        config.set(path + ".x", location.getX());
+        config.set(path + ".y", location.getY());
+        config.set(path + ".z", location.getZ());
+        config.set(path + ".yaw", location.getYaw());
+        config.set(path + ".pitch", location.getPitch());
+        saveConfig();
+    }
+
+    private Location getSpawnLocation(World world) {
+        String path = "spawn.worlds." + world.getName();
+        double x = config.getDouble(path + ".x");
+        double y = config.getDouble(path + ".y");
+        double z = config.getDouble(path + ".z");
+        float yaw = (float)config.getDouble(path + ".yaw");
+        float pitch = (float) config.getDouble(path + ".pitch");
+        return new Location(world, x, y, z, yaw, pitch);
+    }
+
+    private void initConfig() {
+        config = this.getConfig();
+
+        List<World> worlds = Bukkit.getWorlds();
+
+        MemoryConfiguration defaultConfig = new MemoryConfiguration();
+
+        defaultConfig.set("overrideBedSpawn", false);
+
+        for(World world : worlds) {
+            Location defaultLocation = world.getSpawnLocation();
+
+            String path = "spawn.worlds." + world.getName();
+            defaultConfig.set(path + ".x", defaultLocation.getX());
+            defaultConfig.set(path + ".y", defaultLocation.getY());
+            defaultConfig.set(path + ".z", defaultLocation.getZ());
+            defaultConfig.set(path + ".yaw" , 0f);
+            defaultConfig.set(path + ".pitch" , 0f);
+        }
+
+        config.setDefaults(defaultConfig);
+        config.options().copyDefaults(true);
+        saveConfig();
     }
 }
